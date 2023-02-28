7b85c1514a723044661904b7646ccf031a022c9d
emirjunushov <emirjunushov2004@gmail.com>
2023-02-28 10:57:26 +0600

    modal-inputstyle

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 let API = "http://localhost:8000/hakaton";
+// !для фокуса не трогай
+function focusOutFunction(e) {
+  if (e.value.length > 0) {
+    let nameLabel = document.querySelectorAll("#name-label");
+    nameLabel.forEach((item) => {
+      item.style.top = "-19px";
+      item.style.left = "-19px";
+    });
+  }
+}
+// !для фокуса не трогай
